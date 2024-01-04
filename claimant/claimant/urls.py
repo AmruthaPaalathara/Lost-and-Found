6f75b0b12d35c79c428254dca272a53067b2d94e
Amruthapaalathara <amrutha.paalathara@msds.christuniversity.in>
2024-01-04 23:21:08 +0530

    updated claimant html page

--- claimant/claimant/urls.py
+++ claimant/claimant/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from claimant_app import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('claimant/',views.claimant),
+    path('claimant-home/',views.claimant_home)
 ]
--- claimant/claimant_app/views.py
+++ claimant/claimant_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def claimant(request):
+    return render(request,'claimant-home.html')
+
+def claimant_home(request):
+    return render(request,'claimant-form.html')
