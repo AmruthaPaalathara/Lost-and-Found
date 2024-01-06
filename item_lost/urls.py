33ef6e31090ab36ac63016eb1ecc7e1ee43fd1b6
Dipanwita Das <dipanwita.das@science.christuniversity.in>
2024-01-06 09:18:43 +0530

    fixing merge conflits

--- item_lost/urls.py
+++ item_lost/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
 from item_lost import views
 
 urlpatterns=[
     path('',views.item_lost,name="lost"),
-=======
-from item_found import views
-
-urlpatterns=[
     path('found',views.item_found,name='found')
-    
->>>>>>> 11c233bf5ed04b2dc4ad07dce094d7598984da13
 ]
