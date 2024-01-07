6b50751b7b4e3cf3d9bd1c5d828128c7470d2ddd
Sid0702 <siddharth.nautiyal@msds.christ.in>
2024-01-07 21:22:00 +0530

    login pages

--- item_lost/urls.py
+++ item_lost/urls.py
@@ -3,9 +3,5 @@ from item_lost import views
 
 urlpatterns=[
     path('',views.item_lost,name="lost"),
-    path('found',views.item_found,name='found')
-<<<<<<< HEAD
-
-=======
->>>>>>> 33ef6e31090ab36ac63016eb1ecc7e1ee43fd1b6
+    path('found',views.item_found,name='found'),
 ]
