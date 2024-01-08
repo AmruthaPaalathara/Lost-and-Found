60ffa1a9b639f8f19e7e30a662d9a5cacf0ac764
Dipanwita Das <dipanwita.das@science.christuniversity.in>
2024-01-08 13:29:11 +0530

    adding homepage

--- item_lost/urls.py
+++ item_lost/urls.py
@@ -3,5 +3,5 @@ from item_lost import views
 
 urlpatterns=[
     path('',views.item_lost,name="lost"),
-    path('found',views.item_found,name='found'),
+    path('lost',views.item_lost,name='lost'),
 ]
