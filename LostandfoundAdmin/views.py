1336b2928eb4095e5b1260bf6b3e35dbc6b55558
Sid0702 <siddharth.nautiyal@msds.christ.in>
2024-01-27 16:10:42 +0530

    added the html file

--- LostandfoundAdmin/views.py
+++ LostandfoundAdmin/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login , logout
+
 
 # Create your views here.
+
+def login_form(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+        
+
+    return render(request,'Login/login.html')
--- claimant/views.py
+++ claimant/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate , login , logout
 
 # Create your views here.
 def claimant(request):
@@ -8,11 +6,3 @@ def claimant(request):
 
 def claimant_form(request):
     return render(request,'found/claimant-form.html')
-
-def login_form(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-
-    return render(request,'login/login.html')
